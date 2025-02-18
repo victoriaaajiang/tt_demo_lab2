314a139be92d65af7c22e3425a6f6f2cdd3b05f7
SRIRAMNARAYAN KOUSHIK CHITRAPU <koushikcsn@gmail.com>
2025-02-18 17:00:45 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,9 +17,11 @@ module tt_um_adder_TT10_digitalLogic (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = ui_in + ui_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = ui_in - uio_in;
+  assign uio_oe  = 8'b1;
+  assign uio_in= 0;
+  
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
